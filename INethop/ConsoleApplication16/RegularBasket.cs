fa2f531c1f53110f92891aeb5ab9c524a703c2cf
itambu <ivan.ignatenko@gmail.com>
2020-03-14 13:21:22 +0300

    InetShop example

---
+++ INethop/ConsoleApplication16/RegularBasket.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleApplication16.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication16
+{
+    public class RegularBasket : Basket, IBasket
+    {
+        public RegularBasket(ICollection<ISaleItem> items) : base(items)
+        {
+        }
+
+        public double Total
+        {
+            get
+            {
+                return (Items != null) ? Items.Sum(x => x.Total) : 0;
+            }
+        }
+    }
+}
